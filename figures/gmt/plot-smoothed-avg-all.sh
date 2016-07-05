ef0907fd916a23cc5ad4abd0bc9425ef21f864d3
Ricardo Taborda <ricardotaborda@gmail.com>
2016-07-05 11:19:13 -0500

    figure 11 ready

--- figures/gmt/plot-smoothed-avg-all.sh
+++ figures/gmt/plot-smoothed-avg-all.sh
@@ -1,10 +1,7 @@
 
-FILEONE=$1
-FILETWO=$2
+KEY=$1
 CPTFILE=tmp.cpt
 OUTFILE=tmp.ps
-GRDFILE1=tmp1.grd
-GRDFILE2=tmp2.grd
 GRDFILE=tmp.grd
 
 gmt gmtset FONT                     8
@@ -20,23 +17,24 @@ gmt gmtset MAP_TICK_LENGTH_PRIMARY  6p/3p
 gmt gmtset MAP_LABEL_OFFSET         8p
 gmt gmtset MAP_ANNOT_OFFSET_PRIMARY 5p
 
-# gmt makecpt -CcbcRdPu -Do -T0/1/0.05 > ${CPTFILE}
-gmt makecpt -CcbcGreens -Do -T0/1/0.05 > ${CPTFILE}
+gmt makecpt -Ces_skywalker_18 -Do -T0/1/0.05 > ${CPTFILE}
 
-gmt surface ${FILEONE}.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -G${GRDFILE1}
-gmt surface ${FILETWO}.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -G${GRDFILE2}
+gmt surface         pga_${KEY}_50_mean_m_55.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -Gtmp1.grd
+gmt surface         pga_${KEY}_50_mean_m_65.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -Gtmp2.grd
+gmt surface uniform_pga_${KEY}_50_mean_m_55.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -Gtmp3.grd
+gmt surface uniform_pga_${KEY}_50_mean_m_65.txt -R43.5/61.5/34/40 -I0.01 -T0.3 -Gtmp4.grd
 
-gmt grdmath ${GRDFILE2} ${GRDFILE1} ADD 0.5 MUL = ${GRDFILE}
+gmt grdmath tmp1.grd tmp2.grd ADD tmp3.grd ADD tmp4.grd ADD 0.25 MUL = ${GRDFILE}
 
 gmt grdimage ${GRDFILE} -R -JM5i -C${CPTFILE} -K -P -V > ${OUTFILE}
 gmt grdcontour ${GRDFILE} -J -R -A0.2+f6p -W0.5p -O -K -V >> ${OUTFILE}
 gmt psscale -C${CPTFILE} -D5.3i/1i/1.8i/0.15i -E -B0.1 -O -V >>${OUTFILE}
 
-gmt ps2raster ${OUTFILE} -F${FILEONE}-${FILETWO}.pdf -Tf -Au0.5c 
+gmt ps2raster ${OUTFILE} -Fpga-uniform-${KEY}-all-avg.pdf -Tf -Au0.5c 
 
 gmt grdimage ${GRDFILE} -R43.5/61.5/34/40 -JM5i -C${CPTFILE} -P -V > ${OUTFILE}
 
-gmt ps2raster ${OUTFILE} -F${FILEONE}-${FILETWO}.png -Tg -Au
+gmt ps2raster ${OUTFILE} -Fpga-uniform-${KEY}-all-avg.png -Tg -Au
 
 rm tmp.cpt
 rm tmp.grd tmp1.grd tmp2.grd
@@ -45,4 +43,4 @@ rm ${OUTFILE}
 # Opening PDF
 # ------------------------------------------------------------------------------
 
-open -a Adobe\ Acrobat\ Pro.app ${FILEONE}-${FILETWO}.pdf
+open -a Adobe\ Acrobat\ Pro.app pga-uniform-${KEY}-all-avg.pdf
