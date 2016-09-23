3f96a740d1cca355be7f53037c14961b407e9702
khoshnevis.naeem@gmail.com <khoshnevis.naeem@gmail.com>
2016-09-23 12:14:36 -0500

    Some comments addressed. Modified Figure1 to 2 degrees buffer zone. Figure2: modified seismicity.sh, updated historical.txt and instrumental.txt

--- figures/gmt/seismicity.sh
+++ figures/gmt/seismicity.sh
@@ -35,7 +35,7 @@ gmt grdmath mytopo.hist 3 DIV = mytopo.norm
 # Top Map - Historical
 # ------------------------------------------------------------------------------
 
-gmt psbasemap -R43.5/61.5/34/40 -JM5i -BWnes -Ba2f1 -Y8i -V -P -K > ${PSNAME}
+gmt psbasemap -R41.5/63.5/32/42 -JM5i -BWnes -Ba2f1 -Y8i -V -P -K > ${PSNAME}
 
 # gmt psbasemap -R -J -BWnes -Ba2f1 -Y-3.5i -V -P -O -K >> ${PSNAME}
 gmt grdimage mytopo.grd -R -J -Bwnes -Imytopo.norm -Cgray -t50 -V -O -K >> ${PSNAME}
@@ -49,7 +49,7 @@ gmt psxy data.txt -R -J -Sc -W0.5 -Chistorical.cpt -V -K -O >> ${PSNAME}
 # Bottom Map - Instrumental
 # ------------------------------------------------------------------------------
 
-gmt psbasemap -R -J -BWneS -Ba2f1 -Y-2.5i -V -P -O -K >> ${PSNAME}
+gmt psbasemap -R -J -BWneS -Ba2f1 -Y-3.2i -V -P -O -K >> ${PSNAME}
 gmt grdimage mytopo.grd -R -J -Bwnes -Imytopo.norm -Cgray -t50 -V -O -K >> ${PSNAME}
 gmt pscoast -R -J -B -Dh -W0.25p -N1/0.25p -A500 -V -P -O -K >> ${PSNAME}
 gmt pscoast -R -J -B -Slightblue -A500 -Dh -W0.25p -V -P -O -K >> ${PSNAME}
@@ -63,24 +63,24 @@ gmt psxy data.txt -R -J -Sc -W0.5 -G0/220/140 -V -K -O >> ${PSNAME}
 cat << END | \
 awk '{print $2,$3,$1,($4^3.2)/1000}' | \
 gmt psxy -R -J -N -Sc -W0.5 -Chistorical.cpt -V -K -O >> ${PSNAME}
-1900   63	37.5	5.0
-1900   63	36.7	6.0
-1900   63	35.7	7.0
-1900   63	34.5	8.0
+1900   65	37.5	5.0
+1900   65	36.7	6.0
+1900   65	35.7	7.0
+1900   65	34.5	8.0
 END
 
 gmt pstext -R -J -B -D5p/0p -N -V -K -O >> ${PSNAME} << END
-62.1 38.3 8 0 1 ML Magnitude
-63.6 37.5 8 0 1 ML 5.0
-63.6 36.7 8 0 1 ML 6.0
-63.6 35.7 8 0 1 ML 7.0
-63.6 34.5 8 0 1 ML 8.0
+64.1 38.3 8 0 1 ML Magnitude
+65.6 37.5 8 0 1 ML 5.0
+65.6 36.7 8 0 1 ML 6.0
+65.6 35.7 8 0 1 ML 7.0
+65.6 34.5 8 0 1 ML 8.0
 END
 
 # Year Color Scale
 # ------------------------------------------------------------------------------
 
-gmt psscale -Chistorical.cpt -D5.3i/3.5i/1.8i/0.2i -B300 -E -O >> ${PSNAME}
+gmt psscale -Chistorical.cpt -D5.3i/4.2i/1.8i/0.2i -B300 -E -O >> ${PSNAME}
 
 # Creating PDF
 # ------------------------------------------------------------------------------
